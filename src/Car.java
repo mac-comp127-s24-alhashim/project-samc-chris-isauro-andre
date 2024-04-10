b6e9dafd900ae7c87eebb6b7862153ef2e745655
Mohee-San <150849872+Mohee-San@users.noreply.github.com>
2024-04-10 10:43:58 -0500

    Made some variables for car class
    bush da code.

--- src/Car.java
+++ src/Car.java
@@ -4,19 +4,41 @@ private Engine engineType;
 private Tyre tyreType;
 private Racer racer;
 
+private double centerX;
+private double centerY;
 private int acceleration;
 private int topSpeed;
-private int currentSpeed;
+private double currentSpeed = 0;
+private double velocityX;
+private double velocityY;
+
 private final int friction = 0; // 0 is place holder
 
-    public Car(Engine engine, Tyre tyre, Racer racer){
+    public Car(
+        Engine engine, 
+        Tyre tyre, 
+        Racer racer,
+        double centerX,
+        double centerY,
+        double angle){
+
         engineType = engine;
         tyreType = tyre;
         this.racer = racer;
-        acceleration = 0;
-        currentSpeed = 0;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.velocityX = currentSpeed * Math.cos(Math.toRadians(angle));
+        this.velocityY = currentSpeed * -Math.sin(Math.toRadians(angle));
     }
 
+    public void updateAngle(double angle){
+        
+    }
+
+    public boolean updateSpeed(double dt){
+        return true;
+    }
+    
     public void accelerate(){
         // currentSpeed;
     }
