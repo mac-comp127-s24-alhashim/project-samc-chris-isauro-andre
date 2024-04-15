e568c54b1ecb7d99e36dd8524e1a28c83aad0dd0
UpdateJammer <156701322+UpdateJammer@users.noreply.github.com>
2024-04-15 10:27:26 -0500

    Update Tyre.java

--- src/Tyre.java
+++ src/Tyre.java
@@ -6,15 +6,14 @@ public class Tyre {
 
     private ArrayList<Image> wheels;
     private Image wheel;
-    private double turningValue;
+    private double turningValue, friction;
     private String tireType;
-
+    private int grip, ability;
 
     public Tyre(
-    // String tireType, 
-    // double turningValue, 
+    String tireType, 
     ArrayList<Image> wheelImages, 
-    double wheelX, double wheelY
+    double wheelX, double wheelY, double friction, int grip, int ability
     )
     {
         // this.tireType = tireType;
@@ -27,6 +26,14 @@ public class Tyre {
 
     public Image getWheelModel(){
         return wheel;
+    }
+
+    public double getFriction() {
+        return friction;
+    }
+
+    public int getGrip() {
+        return grip;
     }
 
 } 
