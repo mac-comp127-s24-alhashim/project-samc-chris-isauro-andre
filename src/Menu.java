51f4c118b4d47e97797c5d70acf7e4aaa22fc0b2
Chris Frantz <156847502+cfrantz10@users.noreply.github.com>
2024-04-14 19:36:35 -0500

    put some menu buttons in

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,6 @@
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Image;
+import edu.macalester.graphics.ui.Button;
 
 import java.util.ArrayList;
 
@@ -53,24 +54,70 @@ public class Menu {
         
     }
 
-    private void createStartButton(){
-
+    private void createStartButton(){ 
+        Button startButton = new Button("Start");
+        startButton.setPosition(canvas.getWidth()*6/7, canvas.getHeight()*5/7);
+        startButton.setScale(3);
+        canvas.add(startButton);
     }
 
-    private void createTrackButtons(){
+    private void createTrackButtons(){ 
+        Button track1Button = new Button("Track 1");
+        track1Button.setPosition(canvas.getWidth()/12, canvas.getHeight()*2/5);
+        track1Button.setScale(3);
+        canvas.add(track1Button);
 
+        Button track2Button = new Button("Track 2");
+        track2Button.setScale(3);
+        track2Button.setPosition(canvas.getWidth()/12, canvas.getHeight()*3/5);
+        canvas.add(track2Button);
+
+        Button track3Button = new Button("Track 3");
+        track3Button.setScale(3);
+        track3Button.setPosition(canvas.getWidth()/12, canvas.getHeight()*4/5);
+        canvas.add(track3Button);
     }
 
     private void createRacerButtons(){
+        Button racer1Button = new Button("Racer 1");
+        racer1Button.setPosition(canvas.getWidth()/4, canvas.getHeight()*2/5);
+        canvas.add(racer1Button);
 
+        Button racer2Button = new Button("Racer 2");
+        racer2Button.setPosition(canvas.getWidth()/4, canvas.getHeight()*3/5);
+        canvas.add(racer2Button);
+
+        Button racer3Button = new Button("Racer 3");
+        racer3Button.setPosition(canvas.getWidth()/4, canvas.getHeight()*4/5);
+        canvas.add(racer3Button);
     }
 
-    private void createTireButtons(){
-        
+    private void createTireButtons(){ 
+        Button softButton = new Button("Soft");
+        softButton.setPosition(canvas.getWidth()*5/11, canvas.getHeight()*2/5);
+        canvas.add(softButton);
+
+        Button mediumButton = new Button("Medium");
+        mediumButton.setPosition(canvas.getWidth()*5/11, canvas.getHeight()*3/5);
+        canvas.add(mediumButton);
+
+        Button hardButton = new Button("Hard");
+        hardButton.setPosition(canvas.getWidth()*5/11, canvas.getHeight()*4/5);
+        canvas.add(hardButton);
     }
 
-    private void createEngineButtons(){
-        
+    private void createEngineButtons(){ 
+        Button engine1Button = new Button("Engine 1");
+        engine1Button.setPosition(canvas.getWidth()*7/11, canvas.getHeight()*2/5);
+        canvas.add(engine1Button);
+
+        Button engine2Button = new Button("Engine 2");
+        engine2Button.setPosition(canvas.getWidth()*7/11, canvas.getHeight()*3/5);
+        canvas.add(engine2Button);
+
+        Button engine3Button = new Button("Engine 3");
+        engine3Button.setPosition(canvas.getWidth()*7/11, canvas.getHeight()*4/5);
+        canvas.add(engine3Button);
     }
 
     public static void main(String[] args) {
