1b697ea3a336534bd5d08970d7ace0cfc84e26d0
Mohee-San <150849872+Mohee-San@users.noreply.github.com>
2024-04-17 10:07:41 -0500

    flushed out the car class.
    car class almost ready for implementation

--- src/Car.java
+++ src/Car.java
@@ -8,10 +8,11 @@ private Racer racer;
 private Image carModel;
 
 private int topSpeed;
-private double currentSpeed = 0;
+private double currentSpeed;
+private double currentAngle;
 private double velocityX, velocityY;
 
-private final double decelarationFriction = .3;
+private final double passiveDeceleration = .3;
 
     public Car(
         Image carModel,
@@ -26,33 +27,64 @@ private final double decelarationFriction = .3;
         tyre = tyreType;
         racer = racerType;
         this.carModel = carModel;
+
         carModel.setRotation(angle);
         carModel.setPosition(centerX, centerY);
         tyre.getWheelModel().setPosition(centerX, centerY);
         carModel.setScale(.5);
         
-        
-        this.velocityX = currentSpeed * Math.cos(Math.toRadians(angle));
-        this.velocityY = currentSpeed * -Math.sin(Math.toRadians(angle));
+        currentAngle = angle;
+        currentSpeed = 0;
     }
 
+    // Getter methods belowimportant for the movement of the canvas based on the X and Y velocities of the car
+
+    public double getVelocityX(){
+        return velocityX;
+    }
+
+    public double getVelocityY(){
+        return velocityY;
+    }
+
+    // puts the car physically in the canvas
     public void addtoCanvas(CanvasWindow canvas){
         canvas.add(carModel);
         canvas.add(tyre.getWheelModel());
-        
     }
 
+    //rotates car
     public void updateAngle(double angle){
-        double rotationAngle = angle + 1;
+        double rotationAngle = angle;
         carModel.rotateBy(rotationAngle);
         tyre.getWheelModel().rotateBy(rotationAngle);
+        currentAngle = rotationAngle;
     }
 
-    void updateSpeed(double dt){
-        currentSpeed += dt;
+    //speeds up car
+    public void speedUp(){
+        if(currentSpeed <= topSpeed){
+            currentSpeed = currentSpeed + accelarate();
+        }
+        updateSpeed();
     }
 
-    public void decelerate(){
+    public void speedDown(){
+        if(currentSpeed >= 0){
+            currentSpeed = currentSpeed - racer.getBrakepower();
+        }
+        updateSpeed();
     }
 
+    private double accelarate(){
+        double accelaration;
+        accelaration = engine.getTorque() - (tyre.getFriction() * (racer.getWeight() + engine.getWeight()) * 9.8);
+        return accelaration;
+    }
+    
+    private boolean updateSpeed(){
+        velocityX = currentSpeed * Math.cos(Math.toRadians(currentAngle));
+        velocityY = currentSpeed * -Math.sin(Math.toRadians(currentAngle));
+        return true;
+    }
 }
