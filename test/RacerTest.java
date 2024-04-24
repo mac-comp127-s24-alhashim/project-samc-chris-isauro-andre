d9eff849768af6cf739ef4296fd22a1eebfb4f1b
UpdateJammer <156701322+UpdateJammer@users.noreply.github.com>
2024-04-24 10:49:39 -0500

    Worked on RacerTest
    Stuff isn't being added to the canvas for some reason......

--- test/RacerTest.java
+++ test/RacerTest.java
@@ -3,7 +3,7 @@ import java.io.File;
 import edu.macalester.graphics.*;
 import edu.macalester.graphics.events.Key;
 
-public class mechanicsTesting {
+public class RacerTest {
 
     private CanvasWindow canvas;
     private Car car;
@@ -15,27 +15,24 @@ public class mechanicsTesting {
         game.gameStart();
     }
 
-    public mechanicsTesting(){
+    public RacerTest(){
         canvas = new CanvasWindow("PROJECT TEST", 1240,700);
 
     }
 
     public void gameStart(){
-        final double carPositionX = canvas.getWidth()/3;
-        final double carPositionY = canvas.getHeight()/3;
+        final double carPositionX = canvas.getWidth() / 2;
+        final double carPositionY = canvas.getHeight() / 2;
+        Engine engine = new Engine(null, 200, 240, 30);
+        Tire tire = new Tire(null, new File("res/images/WheelImages/tireSoft"), "images/WheelImages/tireSoft1.png",  carPositionX, carPositionY, .2, carAngle, scale, 50, 10);        
+        Racer racer = new Racer(null, "images/driver-body1.png", 20, 15);
+        
+        Track track = new Track("Barcelona", new Image("images/TrackBaseImages/barcelona.jpg"), 70, 60, 20, 1);
+        
+        Car car = new Car(engine, tire, racer, carPositionX, carPositionY, carAngle, scale);
 
-        // car = new Car(
-        //     new Engine(200, 240, 30),
-        //     new Tire(new File("res/images/WheelImages/tireSoft"),
-        //     "images/WheelImages/tireSoft1.png", 
-        //         carPositionX, carPositionY, 
-        //         .2, carAngle, scale, 50, 10),
-        //     new Racer("images/driver-body1.png", 20, 15),
-        //     carPositionX, carPositionY,
-        //     carAngle, scale
-        // );
-    
-        // car.addtoCanvas(canvas);
+        track.addMaptoCanvas(canvas);
+        car.addCarToCanvas(canvas);
         canvas.draw();
 
         canvas.animate(() -> {
