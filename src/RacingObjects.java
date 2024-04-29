9ba2ae0c6afe58d0c9faa658e0645f31a4d35c81
SpamuelButler <scutler1@macalester.edu>
2024-04-28 19:02:11 -0500

    Added code comments to RacerObject.java

--- src/RacingObjects.java
+++ src/RacingObjects.java
@@ -12,7 +12,9 @@ public class RacingObjects {
     private Map<String, Tire> tires = new HashMap<String, Tire>();
     private Map<String, Engine> engines = new HashMap<String, Engine>();
 
-
+    /**
+     * When the object is created all of the objects and maps are automatically created.
+     */
     public RacingObjects(){
         createTracks();
         createRacers();
@@ -20,6 +22,9 @@ public class RacingObjects {
         createEngines();
     }
 
+    /**
+     * Creates all of the tracks. The key String to the Track objects are the same as the trackKey used to create the Track objects.
+     */
     private void createTracks(){
         tracks.put("Suzuka", 
             new Track("Suzuka", new Image("images/TrackBaseImages/suzuka.png"), 5, 2, 6, 8));
@@ -29,12 +34,18 @@ public class RacingObjects {
             new Track("Barcelona", new Image("images/TrackBaseImages/barcelona.png"), 70, 60, 20, 1));
     }
 
+    /**
+     * Creates all of the racers. The key String to the Racer objects are the same as the racerKey used to create the Racer objects.
+     */
     private void createRacers(){
         racers.put("Max",  new Racer("Max", "images/driver-body1.png", 5, 0.67));
         racers.put("Lewis", new Racer("Lewis", "images/driver-body2.png", 2, 0));
         racers.put("Yuki",  new Racer("Yuki", "images/driver-body3.png", 4, 24));
     }
 
+    /**
+     * Creates all of the tires. The key String to the Tire objects are the same as the tireKey used to create the Tire objects.
+     */
     private void createTires(){
         tires.put("Soft", new Tire(
             "Soft",
@@ -50,6 +61,9 @@ public class RacingObjects {
             0.0, 0.0, 0.6, 0.0, 0.4, 5, 100));
     }
 
+    /**
+     * Creates all of the engines. The key String to the Engine objects are the same as the engineKey used to create the Engine objects.
+     */
     private void createEngines(){
         engines.put("V6", new Engine("V6", 0.57, 32, 1));
         engines.put("V10", new Engine("V10", 0.85, 40, 5));
@@ -71,6 +85,4 @@ public class RacingObjects {
     public Map<String, Engine> getEngines() {
         return engines;
     }
-    
-    
 }
